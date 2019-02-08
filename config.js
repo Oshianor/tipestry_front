0398ae482e34434d85b5387dd88908bfec2c16e8
oshianor <abundanceoshianor@gmail.com>
2019-02-08 18:43:42 +0100

    cn push

--- config.js
+++ config.js
@@ -20,12 +20,22 @@ export const config = {
 	// port: 8008
 
 	// live
-	api: "https://tipestry.com/api",
-	url: "https://tipestry.com",
-	host: "https://tipestry.com",
-	profileimage: "https://tipestry.com/public/profile_images/",
+	// api: "https://tipestry.com/api",
+	// url: "https://tipestry.com",
+	// host: "https://tipestry.com",
+	// profileimage: "https://tipestry.com/public/profile_images/",
+	// base64: 'data:image/jpeg;base64,',
+	// topic: "https://tipestry.com/public",
+	// download: 'https://tipestry.com/public/downloads/',
+	// port: 4001
+
+	// cn
+	api: "https://cn.tipestry.com/api",
+	url: "https://cn.tipestry.com",
+	host: "https://cn.tipestry.com",
+	profileimage: "https://cn.tipestry.com/public/profile_images/",
 	base64: 'data:image/jpeg;base64,',
-	topic: "https://tipestry.com/public",
-	download: 'https://tipestry.com/public/downloads/',
-	port: 4001
+	topic: "https://cn.tipestry.com/public",
+	download: 'https://cn.tipestry.com/public/downloads/',
+	port: 5001
 }
