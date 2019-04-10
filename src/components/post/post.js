87e6fb92c886c10297020d6e24d9f61c78e4ee42
Oshianor <abundanceoshianor@gmail.com>
2019-04-10 19:47:34 +0100

    added hot post and recent post feature

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -225,10 +225,13 @@ class Post extends React.Component {
                         width: '100%'
                       }}
                       src={
-                        typeof topic.sites[0] !== "undefined" && this.checkForGif(topic.sites[0].url) == 'gif' || this.checkForGif(topic.sites[0].url) == 'png' || this.checkForGif(topic.sites[0].url) == 'jpg' ?
-                          topic.sites[0].url
+                        typeof topic.sites[0] !== "undefined" ? 
+                            this.checkForGif(topic.sites[0].url) == 'gif' || this.checkForGif(topic.sites[0].url) == 'png' || this.checkForGif(topic.sites[0].url) == 'jpg' ?
+                              topic.sites[0].url
+                            :
+                              "//image.thum.io/get/auth/3228-www.tipestry.com/" + topic.sites[0].url
                         :
-                          "//image.thum.io/get/auth/3228-www.tipestry.com/" + topic.sites[0].url
+                          "https://tipestry.com/public/profile_images/1543028112_tokensale-logo.png"
                       }  
                     />
                   </a>
