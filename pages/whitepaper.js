d7266b3affbddfe5e05f821c3fb3145a739e662a
Oshianor <abundanceoshianor@gmail.com>
2020-02-04 01:25:54 +0100

    reply voting

--- pages/whitepaper.js
+++ pages/whitepaper.js
@@ -1,13 +1,13 @@
-// import React, { Component } from 'react';
-// // import Pdf from "";
+import React, { Component } from 'react';
+// import Pdf from "";
 
-// class Whitepaper extends Component {
-// 	componentDidMount = () => {
-// 		window.open("/static/Tipestry-White-Paper.pdf");
-// 	}
-// 	render() {
-// 		return <a href="/static/Tipestry-White-Paper.pdf" target="_blank" />;
-// 	}
-// }
+class Whitepaper extends Component {
+	componentDidMount = () => {
+		window.open("/static/Tipestry-White-Paper.pdf");
+	}
+	render() {
+		return <a href="/static/Tipestry-White-Paper.pdf" target="_blank" />;
+	}
+}
 
-// export default Whitepaper;
+export default Whitepaper;
