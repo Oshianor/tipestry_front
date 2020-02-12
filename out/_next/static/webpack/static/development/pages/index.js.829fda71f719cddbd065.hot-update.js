b1d893353e05b1627d1ea95fd36ed63f684634d4
Oshianor <abundanceoshianor@gmail.com>
2020-02-12 16:12:02 +0100

    removing preloader

---
+++ out/_next/static/webpack/static/development/pages/index.js.829fda71f719cddbd065.hot-update.js
@@ -0,0 +1,51 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./config.js":
+/*!*******************!*\
+  !*** ./config.js ***!
+  \*******************/
+/*! exports provided: config */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "config", function() { return config; });
+var config = {
+  // dev
+  api: "http://localhost:4000/api",
+  url: "http://localhost:4000",
+  host: "http://localhost:8008",
+  profileimage: "http://localhost:4000/profile_images/",
+  base64: 'data:image/jpeg;base64,',
+  topic: "http://localhost:4000",
+  port: 8008 // demo
+  // api: "http://demo.tipestry.com/api",
+  // url: "http://demo.tipestry.com",
+  // host: "http://demo.tipestry.com",
+  // profileimage: "http://demo.tipestry.com/public/profile_images/",
+  // base64: 'data:image/jpeg;base64,',
+  // topic: "http://demo.tipestry.com/public",
+  // port: 8008
+  // live
+  // api: "https://tipestry.com/api",
+  // url: "https://tipestry.com",
+  // host: "https://tipestry.com",
+  // profileimage: "https://tipestry.com/public/profile_images/",
+  // base64: 'data:image/jpeg;base64,',
+  // topic: "https://tipestry.com/public",
+  // port: 4001
+  // cn
+  // api: "https://cn.tipestry.com/api",
+  // url: "https://cn.tipestry.com",
+  // host: "https://cn.tipestry.com",
+  // profileimage: "https://cn.tipestry.com/public/profile_images/",
+  // base64: 'data:image/jpeg;base64,',
+  // topic: "https://cn.tipestry.com/public",
+  // port: 5001
+
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.829fda71f719cddbd065.hot-update.js.map
